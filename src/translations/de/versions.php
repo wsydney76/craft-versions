503b8ac8aa673d67c1a4f943060daa9d78c6f4f8
wsydn <wsydney@web.de>
2019-07-04 09:15:28 +0200

    Added german translations

---
+++ src/translations/de/versions.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'All' => 'Alle',
+    'By status' => 'Nach Status',
+    'Creator' => 'Ersteller',
+    'Date created' => 'Erstellungsdatum',
+    'Date updated' => 'Änderungsdatum',
+    'Drafts and Revisions' => 'Entwürfe und Revisionen',
+    'Drafts' => 'Entwürfe',
+    'Edit' => 'Bearbeiten',
+    'Notes' => 'Anmerkungen',
+    'Revisions' => 'Revisionen',
+    'Saved' => 'Gespeichert',
+    'Section' => 'Sektion',
+    'Title' => 'Titel',
+    'Unsaved' => 'Ungespeichert',
+    'View' => 'Anschauen',
+];
