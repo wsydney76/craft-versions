54eab1e9886edf7acc865e3350bb28cb12a12066
wsydn <wsydney@web.de>
2019-07-06 20:08:28 +0200

    Added settings, added service/behavior to get complex queries out of twig templates

---
+++ src/services/VersionsService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace wsydney76\versions\services;
+
+use Craft;
+use craft\base\Component;
+use craft\elements\Entry;
+use craft\records\Entry as EntryRecord;
+use wsydney76\versions\Versions;
+
+class VersionsService extends Component
+{
+    /**
+     * @return bool
+     */
+    public function showInternal()
+    {
+
+        $showInternalSetting = Versions::getInstance()->getSettings()->showInternal;
+
+        return $showInternalSetting == 'always'
+            || ($showInternalSetting == 'admin' && Craft::$app->user->getIdentity()->admin)
+            || ($showInternalSetting == 'dev' && Craft::$app->config->general->devMode);
+    }
+
+    public function getEntryById(int $id, string $site)
+    {
+        return Entry::find()
+            ->site($site)
+            ->anyStatus()
+            ->id($id)
+            ->one();
+    }
+
+    /**
+     * @param string $site
+     * @param string $preferSite
+     * @return array|\craft\base\ElementInterface[]|Entry[]
+     */
+    public function getAllDrafts(string $site, string $preferSite)
+    {
+        return Entry::find()
+            ->site($site)
+            ->unique()
+            ->preferSites([$preferSite])
+            ->anyStatus()
+            ->drafts(true)
+            ->orderBy('dateCreated desc')
+            ->all();
+    }
+
+}
