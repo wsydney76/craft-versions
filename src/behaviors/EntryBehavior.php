a5fc280fca849d18bf584156d509d8c033e063cf
wsydn <wsydney@web.de>
2019-07-25 14:38:10 +0200

    Added more restrictions, moved notifications to entry edit form header

--- src/behaviors/EntryBehavior.php
+++ src/behaviors/EntryBehavior.php
@@ -5,6 +5,7 @@ namespace wsydney76\versions\behaviors;
 use Craft;
 use craft\elements\Entry;
 use craft\helpers\DateTimeHelper;
+use craft\helpers\ElementHelper;
 use craft\records\Entry as EntryRecord;
 use yii\base\Behavior;
 
@@ -46,6 +47,24 @@ class EntryBehavior extends Behavior
             return null;
         }
         return DateTimeHelper::toDateTime($entryRecord->dateCreated);
+    }
 
+    public function canSave()
+    {
+        $entry = $this->owner;
+        if ($entry->propagating || $entry->duplicateOf || $entry->resaving || ElementHelper::isDraftOrRevision($entry)) {
+            return true;
+        }
+
+        if (!Craft::$app->user->can('ignoreVersionsRestrictions')) {
+            $settings = Craft::$app->plugins->getPlugin('versions')->settings;
+            if (isset($settings['allowEditSourceIfDraftsExist']) && !$settings['allowEditSourceIfDraftsExist']) {
+                if (count($this->getDrafts())) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
